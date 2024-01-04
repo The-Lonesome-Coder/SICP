a941b00f6afe901ae47fecbb35f10126d28a9261
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2024-01-04 14:38:42 +0800

    Exercises 1.5

---
+++ Chapter 01/Exercises/1.5.rkt
@@ -0,0 +1,31 @@
+#lang sicp
+
+(define (p) (p))
+
+(define (test x y)
+    (if (= x 0) 0
+                y))
+
+(test 0 (p))
+
+
+; Applicative-order evaluation
+(test 0 (p))
+
+(test 0 (p))
+
+(test 0 (p))
+
+    ; ... Infinite expansion
+
+
+; Normal-order evaluation
+(test 0 (p))
+
+(if (= 0 0) 0
+            (p))
+
+(if #t 0
+       (p))
+
+0
